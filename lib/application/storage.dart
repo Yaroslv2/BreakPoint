5a7db991356179a4be077107172fc9033f18dd80
Yaroslv2 <yaroslv.mudrov@gmail.com>
2023-02-02 22:13:08 +0300

    first commit

---
+++ lib/application/storage.dart
@@ -0,0 +1,42 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Storage {
+  final _key = "token";
+
+  Future<bool> isHaveToken() async {
+    final storage = await SharedPreferences.getInstance();
+    final result = storage.get(_key);
+    return result == null ? false : true;
+  }
+
+  Future<String?> getTokenInStorage() async {
+    final storage = await SharedPreferences.getInstance();
+    final result = storage.getString(_key);
+    return result;
+  }
+
+  Future<void> putTokenInStorage(String token) async {
+    final storage = await SharedPreferences.getInstance();
+    storage.setString(_key, token);
+  }
+
+  Future<void> deleteToken() async {
+    final storage = await SharedPreferences.getInstance();
+    storage.clear();
+  }
+
+  Future<void> saveUser(String _email, String _name) async {
+    final storage = await SharedPreferences.getInstance();
+    storage.setString("email", _email);
+    storage.setString("name", _name);
+  }
+
+  Future<Map<String, String?>> getUser() async {
+    final storage = await SharedPreferences.getInstance();
+    Map<String, String?> result = {
+      "email": storage.getString("email"),
+      "name": storage.getString("name"),
+    };
+    return result;
+  }
+}
---
+++ lib/constants.dart
@@ -0,0 +1,4 @@
+const BackendIP = "192.168.0.19";
+const BackendPort = ":5000";
+
+const BackendUrl = BackendIP + BackendPort;
